1ed7a63d92b6e3f041673b40a3a8718955d79cf5
SergeyMitroshin <sergey@host-best.com>
2022-12-21 22:28:00 +0300

    v05

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # autor Sergey Mitroshin
 
+import func1
+
 command=0
-while 1==1:
+exitflag=0
+while exitflag==0:
     print ('*************************************')
     print (' Текстовая база телефонов')
     print (' Нажмите: ')
     print ('1 Для ввода данных в базу')
-    print ('2 Для поиска записи по фамилии')
+    print ('2 Для вызова функции 1')
     print ('3 Для конвертации базы в формат 1')
     print ('0 Для завершения программы')
     print ('*************************************')
@@ -14,8 +17,11 @@ while 1==1:
         command =int(input())
         if command==1:
             print('1')
+        elif command == 2:
+            func1.fun1()
+            exitflag = 1 # если хотим завершить программу
         elif command == 0:
-            break
+            exitflag = 1
     except:
         print ('Повторите ввод')
 
