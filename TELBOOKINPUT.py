1511b5ce9559afcdfa48b60dbe4170112787426c
SergeyMitroshin <sergey@host-best.com>
2022-12-22 11:32:22 +0300

    v09

---
+++ TELBOOKINPUT.py
@@ -0,0 +1,28 @@
+
+from this import db.txt
+
+def get_surname():
+    surname = input('Фамилия:')
+    return surname
+
+def get_data():
+    surname = input('Фамилия:')
+    name = input('Имя:')
+    number = input('Номер телефона:')
+    description = input('Описание:') == [surname, name, number, description] 
+    return list
+
+def get_name():
+    name = input('Имя:')
+    return name
+
+def get_number():
+    number = input('№ телефона:')
+    return number
+
+def get_description():
+    description = input('Описание')
+    return description
+
+def data_collection():
+    return (get_surname(),get_data(), get_name(), get_number(),get_description())    
